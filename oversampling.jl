32e89b768b7a5ce16e104d8dd9addfe29671d4bb
zholzer <zholzer3693@sdsu.edu>
2024-09-09 11:59:02 -0700

    Cleaning code

--- oversampling.jl
+++ oversampling.jl
@@ -6,48 +6,64 @@ using CSV
 
 # script inputs
 # input a fasta file with ID, Country:Region, [other stuff], date yyyy-mm-dd
-inFile = "Data/Intermediate/Trim/ncbi_fluB_2024-05-24_allDates_USA_trim.fasta"
-
-splitFile = split(inFile, "/")
-splitFile = split(splitFile[end], ".")
-fileName = splitFile[1]
-outFile = "Data/Oversampled/"*fileName*"-os.fasta"
-
+inFile = "ncbi_canineParvovirus_2024-04-15_China_VP2-clean.fasta"
+outFileLocation = ""
 n = 1 # partition by 1, 2, 3, 4, 6, and 12 month groups -> 1=each month, 12=each year
 minSamplesNeeded = 10
 # fixed number for all sampling
-noMonth = "01"
-noDay = "01"
-oversampleBool = true
-undersampleBool = true
+monthIfNoMonth = "01"
+dayIfNoDay = "01"
+oversample = true
+undersample = true
 
-df = DataFrame(ID = String[], Year = Int64[], Month = Int64[], Day = Int64[], Seq = String[]) # initialize dataframe
-FastaReader(inFile) do fr # parse fasta file
-    for (header, seq) in fr
-        splitHeader = split(header, "_") # split header
-        splitDate = split(splitHeader[end], "-") # last input is date
-        if splitDate[1] == "" # if no date skip
-            global noDateCount += 1
-            continue
-        else # otherwise push to dataframe 
-            if length(splitDate) == 3
-                adjustedDate = [splitDate[1], splitDate[2], splitDate[2]]
-            elseif length(splitDate) == 2
-                adjustedDate = [splitDate[1], splitDate[2], noDay]
-            elseif length(splitDate) == 1
-                adjustedDate = [splitDate[1], noMonth, noDay] # where to put samples missing months?
+outFileName = get_outfile_name_from_infile(inFile, outFileLocation)
+
+fastaDF = DataFrame(ID = String[], Year = Integer[], Month = Integer[], Day = Integer[], Seq = String[]) # initialize dataframe
+read_fasta_file(inFile::string, fastaDF::DataFrame, [monthIfNoMonth, dayIfNoDay])
+
+function get_outfile_name_from_infile(inFile::String, outFile::String, missingDateOptions::AbstractVector)
+    splitFile = split(inFile, "/")
+    splitFile = split(splitFile[end], ".")
+    fileName = splitFile[1]
+    outFileName = outFileLocation*fileName*"-os.fasta"
+    return outFileName
+end
+
+function read_fasta_file(inFile::string, fastaDF::DataFrame)
+    FastaReader(inFile) do fr # parse fasta file
+        for (header, seq) in fr
+
+            splitHeader = split(header, "_") # split header
+            splitDate = split(splitHeader[end], "-") # last input is date
+
+            if splitDate[1] == "" # if no date skip
+                global noDateCount += 1
+                continue
+            else # otherwise push to dataframe 
+                push_sequence_to_df(splitDate)
             end
 
-            year = tryparse(Int64, adjustedDate[1])
-            month = tryparse(Int64, adjustedDate[2])
-            day = tryparse(Int64, adjustedDate[3])
-            push!(df, [splitHeader[1], year, month, day, seq])
         end
     end
 end
 
+function push_sequence_to_df(splitDate, missingDateOptions)
+    if length(splitDate) == 3
+        adjustedDate = [splitDate[1], splitDate[2], splitDate[2]]
+    elseif length(splitDate) == 2
+        adjustedDate = [splitDate[1], splitDate[2], missingDateOptions[2]]
+    elseif length(splitDate) == 1
+        adjustedDate = [splitDate[1], missingDateOptions[1], missingDateOptions[2]] # where to put samples missing months?
+    end
+
+    year = tryparse(Integer, adjustedDate[1])
+    month = tryparse(Integer, adjustedDate[2])
+    day = tryparse(Integer, adjustedDate[3])
+    push!(fastaDF, [splitHeader[1], year, month, day, seq])
+end
+
 dfYearMonth = Array{Any}(undef,0)
-dfYear = groupby(df, :Year)
+dfYear = groupby(fastaDF, :Year)
 conditions = Vector{BitVector}(undef, length(1:n:12))
 for frame in dfYear
     let iter = 1
@@ -63,7 +79,7 @@ for frame in dfYear
     end
 end
 
-if (oversampleBool == false) && (undersampleBool == false)
+if (oversample == false) && (undersample == false)
     display("Neither oversampling or undersampling was selected.")
     exit()
 else
@@ -73,7 +89,7 @@ end
 for frame in dfYearMonth
     numRow = size(frame, 1)
 
-    if oversampleBool == true
+    if oversample == true
         let copyNum = 1
             while size(frame, 1) < minSamplesNeeded 
                 let iter = rand(1:numRow)
@@ -86,7 +102,7 @@ for frame in dfYearMonth
         end
     end
 
-    if undersampleBool == true
+    if undersample == true
         let copyNum = 0
             while size(frame, 1) > minSamplesNeeded 
                 let iter = rand(1:numRow - copyNum)
